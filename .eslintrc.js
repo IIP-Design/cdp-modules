6480600326b07345a71e2c1127df9c09678afa37
mrewers <hello@marek.dev>
2022-05-11 10:36:12 -0400

    test: set up TypeScript linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,58 @@ module.exports = {
     'docs/_site/',
     'node_modules/',
   ],
+  overrides: [
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      'extends': [
+        '@gpa-lab/eslint-config',
+        '@gpa-lab/eslint-config/react',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      rules: {
+        'import/extensions': [
+          'error',
+          'always',
+          {
+            'd.ts': 'never',
+            js: 'never',
+            jsx: 'never',
+            ts: 'never',
+            tsx: 'never',
+          },
+        ],
+        'import/no-unassigned-import': [
+          'error',
+          {
+            allow: ['**/*.css', '**/*.scss'],
+          },
+        ],
+        'node/file-extension-in-import': [
+          'error', 'always', { '.js': 'never', '.jsx': 'never', '.ts': 'never', '.tsx': 'never' },
+        ],
+        'react/jsx-filename-extension': [
+          error,
+          {
+            extensions: [
+              '.tsx', '.js'
+            ],
+          },
+        ],
+      },
+      settings: {
+        'import/parsers': {
+          '@typescript-eslint/parser': [
+            '.ts', '.tsx', '.d.ts',
+          ],
+        },
+      },
+    },
+  ],
   parser: '@babel/eslint-parser',
   parserOptions: {
     babelOptions: {
